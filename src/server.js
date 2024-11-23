4dbfe041c6f050174dc9079e2f16fbd4f593694c
natabentosandrade <natabentosandrade@gmail.com>
2024-11-23 20:57:08 +0000

    modified:   src/server.js Rremoved "espress.static" line

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ import route from "./route/postRoute.js";
 // Create an Express application instance
 const app = express();
 
-app.use(express.static("uploads"));
-
 route(app);
 // Start the server on port 3000
 app.listen(3000, () => {
